ff837547f947030a84551de095cb7acfb13b4f40
andregorni <ac.gorni@gmail.com>
2021-05-17 23:03:55 -0300

    Desafio 4 - Logica - VNW

---
+++ index.js
@@ -0,0 +1,36 @@
+function dia(){
+    console.log("Como o dia de hoje foi bom!")
+}
+
+dia()
+
+
+
+function usuario(nome){
+    console.log(`Olá ${nome}`)  
+}
+
+usuario("André")
+
+
+
+function estilom (nome, idade,estilomusical) {
+    console.log(`Olá meu nome é ${nome} tenho ${idade} anos e curto ${estilomusical}`)  
+}
+
+estilom("André", "32", "Rock")
+
+
+
+function filmef (filme, musica) {
+    console.log(`Meu filme favorito é ${filme} e minha música favorita é ${musica}`)  
+}
+
+filmef("Duro de Matar", "BYOB")
+
+
+
+function triploN(num){
+    return num * 3
+}
+console.log(triploN(7))
